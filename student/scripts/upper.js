d0594e0b7e5f16a1168438c0b0670c04cdb6199b
feliasfogg <good-1991@mail.ru>
2014-12-07 19:44:30 +0300

    up from linux

--- student/scripts/upper.js
+++ student/scripts/upper.js
@@ -1,19 +1,19 @@
-$(document).ready(function () {
-    $('#Footer_cont').append("<a class='upper' title='�����'>&nbsp;</a>");
-    $(".upper").css("display", "none");
-    $(window).scroll(function () {
-        if ($(this).scrollTop() >= (parseInt($(document).height()) - parseInt($(window).height()) - 100)) {
-            $(".upper").stop().animate({"bottom": "20px" }, 100);
-            $(".upper").css("display", "block");
-        } else {
-            $(".upper").stop().animate({"bottom": "-50px" }, 100);
-            $(".upper").css("display", "none");
-        }
-    });
-    $(".upper").click(function () {
-        jQuery('html, body').animate({scrollTop: 0}, 'fast');
-        return false;
-    });
-
-
-});
+$(document).ready(function () {
+    $('#Footer_cont').append("<a class='upper' title='�����'>&nbsp;</a>");
+    $(".upper").css("display", "none");
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= (parseInt($(document).height()) - parseInt($(window).height()) - 100)) {
+            $(".upper").stop().animate({"bottom": "20px" }, 100);
+            $(".upper").css("display", "block");
+        } else {
+            $(".upper").stop().animate({"bottom": "-50px" }, 100);
+            $(".upper").css("display", "none");
+        }
+    });
+    $(".upper").click(function () {
+        jQuery('html, body').animate({scrollTop: 0}, 'fast');
+        return false;
+    });
+
+
+});
