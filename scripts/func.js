fd2b04879e959a05c317825d734898255b6e70da
feliasfogg <good-1991@mail.ru>
2014-12-15 16:41:19 +0300

    допилено

--- scripts/func.js
+++ scripts/func.js
@@ -1,20 +1,1 @@
-function highlightTableRows(tableId, hoverClass) {
-    var table = document.getElementById(tableId);
-
-    if (table) {
-        if (hoverClass) {
-            var hoverClassReg = new RegExp("\\b" + hoverClass + "\\b");
-
-            table.onmouseover = table.onmouseout = function (e) {
-                if (!e) e = window.event;
-                var elem = e.target || e.srcElement;
-                while (!elem.tagName || !elem.tagName.match(/td|table/i)) elem = elem.parentNode;
-
-                if (elem.parentNode.tagName == 'TR' && elem.parentNode.parentNode.tagName == 'TBODY') {
-                    var row = elem.parentNode;
-                    if (!row.getAttribute('clickedRow')) row.className = e.type == "mouseover" ? row.className + " " + hoverClass : row.className.replace(hoverClassReg, " ");
-                }
-            };
-        }
-    }
-}
+function highlightTableRows(tableId, hoverClass) {    var table = document.getElementById(tableId);    if (table) {        if (hoverClass) {            var hoverClassReg = new RegExp("\\b" + hoverClass + "\\b");            table.onmouseover = table.onmouseout = function (e) {                if (!e) e = window.event;                var elem = e.target || e.srcElement;                while (!elem.tagName || !elem.tagName.match(/td|table/i)) elem = elem.parentNode;                if (elem.parentNode.tagName == 'TR' && elem.parentNode.parentNode.tagName == 'TBODY') {                    var row = elem.parentNode;                    if (!row.getAttribute('clickedRow')) row.className = e.type == "mouseover" ? row.className + " " + hoverClass : row.className.replace(hoverClassReg, " ");                }            };        }    }}
