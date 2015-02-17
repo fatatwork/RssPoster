8fffe624757c99e6d5fe4dd2919ccbc7b8b3d273
Pavel Chehov <good-1991@mail.ru>
2015-02-17 11:16:45 +0300

    добавлен сам скрипт

--- sample.php
+++ sample.php
@@ -1,4 +1,4 @@
-<script src="//ulogin.ru/js/ulogin.js"></script>
+<script src="/ulogin.js"></script>
 <div id="uLogin" data-ulogin="display=small;fields=first_name,last_name;providers=vkontakte,odnoklassniki,mailru,facebook;hidden=other;redirect_uri=http%3A%2F%2Fbsmu.akson.by%2Fsample.php"></div>
     
     <?php
