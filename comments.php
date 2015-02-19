df538233be014b56c8c3a4c282955342d94ebe86
Pavel Chehov <good-1991@mail.ru>
2015-02-19 21:28:08 +0300

    рефакторинг

--- comments.php
+++
@@ -1,5 +0,0 @@
-<?
-	echo ("Your name is ".$user['first_name']);
-    echo ("Your login with ".$user['network']);
-    echo ("Your id ".$user['identity']);
-?>
--- sample.php
+++ sample.php
@@ -1,4 +1,5 @@
 ﻿<?php 
+     require 'app_config.php'; //подключение файла с данными для входа в БД
      $boolCheckCookie = false;
      if(isset($_COOKIE['first_name'])){ $username['first_name'] = $_COOKIE['first_name']; $boolCheckCookie = true;}
      if(isset($_COOKIE['last_name'])){ $username['last_name'] = $_COOKIE['last_name']; $boolCheckCookie = true;}
@@ -13,20 +14,24 @@
      $comment = trim($_REQUEST['user_comment']);
      if(!$comment) die("Error comment");
      if(isset($username)){
-          $db_name="user1137761_testn";
-          $dbhost = "localhost"; // Имя хоста БД
-          $dbusername = "fatatwork"; // Пользователь БД
-          $dbpass = "nyatech22"; // Пароль к базе
-          $dbconnect = mysql_connect ($dbhost, $dbusername, $dbpass); 
+          
+          //коннект к базе
+          $dbconnect = mysql_connect ($dbhost, $dbusername, $dbpass);
           if (!$dbconnect){ die("<p>Ошибка подключения к базе данных: " . mysql_error() . "</p>"); }
+          //говорим базе что записываем в нее все в utf8
+          mysql_query("SET NAMES 'utf8';"); 
+          mysql_query("SET CHARACTER SET 'utf8';"); 
+          mysql_query("SET SESSION collation_connection = 'utf8_general_ci';");
+
           mysql_select_db($db_name) or die ("<p>Невозможно выбрать базу: " . mysql_error() . "</p>");
 
-          $query = "INSERT INTO comments (firstname, lastname, url, network, comment)" .
-          "VALUES ('{$username['first_name']}', '{$username['last_name']}', '{$username['identity']}', '{$username['network']}', '{$comment}');";
-          $result = mysql_query($query);
-          if(!$result) die("<p>Невозможно сделать запись комментария: " . mysql_error() . "</p>"); 
-          else echo ("Success");
 
+          $query = "INSERT INTO users (first_name, last_name, network, network_url)" .
+          "VALUES ('{$username['first_name']}', '{$username['last_name']}', '{$username['network']}', '{$username['identity']}');";
+          $result = mysql_query($query) or die("<p>Невозможно сделать запись комментария: " . mysql_error() . "</p>"); 
+          
+          $query= "INSERT INTO comments (comment) VALUES ('{$comment}');" or die("<p>Невозможно сделать запись комментария: " . mysql_error() . "</p>");
+          $result = mysql_query($query) or die("<p>Невозможно сделать запись комментария: " . mysql_error() . "</p>");
      } else echo ("Error: user not exist.");
                     //$user['network'] - соц. сеть, через которую авторизовался пользователь
                     //$user['identity'] - уникальная строка определяющая конкретного пользователя соц. сети
