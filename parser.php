c8fd10a881878097bf10ee726c4519e5f020d5dc
Pavel Chehov <good-1991@mail.ru>
2015-02-24 02:03:41 +0300

    прикручены смс

--- parser.php
+++ parser.php
@@ -97,7 +97,9 @@ if($comment_life>=20){
   обсуждение 
   $url?post_add#post_add";
 	mail("good-1991@mail.ru", "Chat", $message);
-  $sms=file_get_contents("http://sms.ru/sms/send?api_id=b8646699-0b12-1c14-ad92-7ab16971b8a1&to=375259466591&text=".urlencode(iconv("windows-1251","utf-8","$url?post_add#post_add")));
+  if($comment_life>=45){
+  $sms=file_get_contents("http://sms.ru/sms/send?api_id=b8646699-0b12-1c14-ad92-7ab16971b8a1&to=375259466591&text=".urlencode(iconv("windows-1251","utf-8","Last comment was added $comment_life minutes ago.$url")));    
+    }
   }
      	    $dbconnect = mysql_connect ($dbhost, $dbusername, $dbpass) or die("<p>Ошибка подключения к базе данных: " . mysql_error() . "</p>");
           //говорим базе что записываем в нее все в utf8
