4cf57fea0513e0d07e8c6f8b317a89269df2c02d
Pavel Chehov <good-1991@mail.ru>
2015-02-26 20:18:27 +0300

    format code

--- parser.php
+++ parser.php
@@ -2,126 +2,159 @@
 <meta charset="UTF-8">
 </html>
 <?php
-chdir("/home/user1137761/www/bsmu.akson.by");
+chdir( "/home/user1137761/www/bsmu.akson.by" );
 require_once 'simplehtmldom/simple_html_dom.php';
 require_once 'app_config.php';
 
-      function searchUser($author_id){//ищем юзера по url возвращаем в качестве результата всю строку row
-          $query="SELECT * FROM vk_users WHERE vk_id='{$author_id}';";
-          $res=mysql_query($query) or die("<p>searchUser Невозможно сделать запрос поиска пользователя: " . mysql_error() . "</p>");
-          $row=mysql_fetch_array($res);//получение результата запроса из базы;
-          return $row;
-     }
-     function addUser($first_name, $last_name, $author_id){//добавление пользователя
-          $query = "INSERT INTO vk_users (first_name, last_name, vk_id)" .
-          "VALUES ('{$first_name}', '{$last_name}', '{$author_id}');";
-          $result = mysql_query($query) or die("<p>Невозможно добавить пользователя " . mysql_error() . "</p>");
-     }
-
-     function addComment($user_id, $comment_life, $comment_time){//добавляем комментарий
-          $query="SELECT comment_time FROM vk_comments WHERE user_id='{$user_id}';";
-          $res=mysql_query($query) or die("<p>searchUser Невозможно сделать запрос поиска комментария для пользователя $user_id: " . mysql_error() . "</p>");
-          $row=mysql_fetch_array($res);//получение результата запроса из базы;
-          if($row){
-            $com_time=end($row);
-            if($com_time!=$comment_time){
-              $query= "INSERT INTO vk_comments (user_id, comment_life, comment_time) VALUES ('{$user_id}', '{$comment_life}', '{$comment_time}');";
-              $res =mysql_query($query) or die("<p>Невозможно сделать запись комментария: " . mysql_error() . "</p>");
-            }
-          } else {
-          $query= "INSERT INTO vk_comments (user_id, comment_life, comment_time) VALUES ('{$user_id}', '{$comment_life}', '{$comment_time}');";
-          $res =mysql_query($query) or die("<p>Невозможно сделать запись комментария: " . mysql_error() . "</p>");
-          }
-          return $res;
-     }
-     function getCommentsCount($u_id){
-     	    $query="SELECT * FROM vk_comments WHERE user_id='{$u_id}';";
-          $res=mysql_query($query) or die("<p>getCommentsCountНевозможно сделать запрос поиска пользователя: " . mysql_error() . "</p>");
-          $row=mysql_fetch_array($res);//получение результата запроса из базы;
-          $rows=mysql_num_rows($res);
-          $query="UPDATE vk_users SET all_comments='{$rows}' WHERE id='$u_id'";
-          $res=mysql_query($query) or die("<p>Невозможно сделать запрос поиска пользователя: " . mysql_error() . "</p>");
-          echo "All = $rows";
-     }
-
-$url="https://m.vk.com/wall-43932139_5970";
-$html = file_get_html($url);
-if(!$html) echo "error";
-$fnd_author=$html->find('a.pi_author');//в масссиве вк всегда 51 коммент
-$fnd_comment=$html->find('div.pi_text');
-for($i=40; $i<count($fnd_author); ++$i){
-  $author=trim($fnd_author[$i]->innertext);
-  if($author=="Официальное сообщество Plantronics"){
-  $message="Message from admin";    
-  mail("good-1991@mail.ru","Chat", $message);
-  mail("feliasfogg1@yandex.ru", "Chat", $message);
-  mail("pavel.felias@gmail.com","Chat", $message);
-  mail("feliasfogg@outlook.com","Chat", $message);
-  break;
-  }
+function searchUser(
+	$author_id
+) {//ищем юзера по url возвращаем в качестве результата всю строку row
+	$query = "SELECT * FROM vk_users WHERE vk_id='{$author_id}';";
+	$res = mysql_query( $query )
+	or die( "<p>searchUser Невозможно сделать запрос поиска пользователя: "
+	        . mysql_error() . "</p>" );
+	$row = mysql_fetch_array( $res );//получение результата запроса из базы;
+	return $row;
 }
 
-$author=end($fnd_author);
-sscanf($author, "<a class=\"pi_author\" href=\"/%s\">", $author_id);
-$author=trim($author->innertext);
-sscanf($author, "%s %s", $first_name, $last_name);
+function addUser(
+	$first_name, $last_name, $author_id
+) {//добавление пользователя
+	$query = "INSERT INTO vk_users (first_name, last_name, vk_id)" .
+	         "VALUES ('{$first_name}', '{$last_name}', '{$author_id}');";
+	$result = mysql_query( $query )
+	or die( "<p>Невозможно добавить пользователя " . mysql_error() . "</p>" );
+}
+
+function addComment(
+	$user_id, $comment_life, $comment_time
+) {//добавляем комментарий
+	$query = "SELECT comment_time FROM vk_comments WHERE user_id='{$user_id}';";
+	$res = mysql_query( $query ) or die( "<p>searchUser Невозможно сделать запрос поиска комментария для пользователя $user_id: "
+	                                     . mysql_error() . "</p>" );
+	$row = mysql_fetch_array( $res );//получение результата запроса из базы;
+	if ( $row ) {
+		$com_time = end( $row );
+		if ( $com_time != $comment_time ) {
+			$query
+				= "INSERT INTO vk_comments (user_id, comment_life, comment_time) VALUES ('{$user_id}', '{$comment_life}', '{$comment_time}');";
+			$res = mysql_query( $query )
+			or die( "<p>Невозможно сделать запись комментария: " . mysql_error()
+			        . "</p>" );
+		}
+	} else {
+		$query
+			= "INSERT INTO vk_comments (user_id, comment_life, comment_time) VALUES ('{$user_id}', '{$comment_life}', '{$comment_time}');";
+		$res = mysql_query( $query )
+		or die( "<p>Невозможно сделать запись комментария: " . mysql_error()
+		        . "</p>" );
+	}
+
+	return $res;
+}
+
+function getCommentsCount( $u_id ) {
+	$query = "SELECT * FROM vk_comments WHERE user_id='{$u_id}';";
+	$res = mysql_query( $query )
+	or die( "<p>getCommentsCountНевозможно сделать запрос поиска пользователя: "
+	        . mysql_error() . "</p>" );
+	$row   = mysql_fetch_array( $res );//получение результата запроса из базы;
+	$rows  = mysql_num_rows( $res );
+	$query = "UPDATE vk_users SET all_comments='{$rows}' WHERE id='$u_id'";
+	$res = mysql_query( $query )
+	or die( "<p>Невозможно сделать запрос поиска пользователя: " . mysql_error()
+	        . "</p>" );
+	echo "All = $rows";
+}
+
+$url  = "https://m.vk.com/wall-43932139_5970";
+$html = file_get_html( $url );
+if ( ! $html ) {
+	echo "error";
+}
+$fnd_author  = $html->find( 'a.pi_author' );//в масссиве вк всегда 51 коммент
+$fnd_comment = $html->find( 'div.pi_text' );
+for ( $i = 40; $i < count( $fnd_author ); ++ $i ) {
+	$author = trim( $fnd_author[ $i ]->innertext );
+	if ( $author == "Официальное сообщество Plantronics" ) {
+		$message = "Message from admin";
+		mail( "good-1991@mail.ru", "Chat", $message );
+		mail( "feliasfogg1@yandex.ru", "Chat", $message );
+		mail( "pavel.felias@gmail.com", "Chat", $message );
+		mail( "feliasfogg@outlook.com", "Chat", $message );
+		break;
+	}
+}
+
+$author = end( $fnd_author );
+sscanf( $author, "<a class=\"pi_author\" href=\"/%s\">", $author_id );
+$author = trim( $author->innertext );
+sscanf( $author, "%s %s", $first_name, $last_name );
 echo "$first_name $last_name </br>";
-$author_id=substr($author_id, 0, strpos($author_id, "\">"));
+$author_id = substr( $author_id, 0, strpos( $author_id, "\">" ) );
 echo "<a href=\"https://vk.com/$author_id\">$author_id<a/></br>";
 
-$fnd=$html->find('a.item_date');
-$comment_time=end($fnd);
-$comment_time=$comment_time->innertext;
-$num=strlen($comment_time);
-$comment_time=substr($comment_time, 18);
+$fnd          = $html->find( 'a.item_date' );
+$comment_time = end( $fnd );
+$comment_time = $comment_time->innertext;
+$num          = strlen( $comment_time );
+$comment_time = substr( $comment_time, 18 );
 echo "$comment_time ";
 
-sscanf($comment_time, "%d:%d", $hour, $min);
-$minLastComm=$hour*60+$min;
+sscanf( $comment_time, "%d:%d", $hour, $min );
+$minLastComm = $hour * 60 + $min;
 echo "$minLastComm </br>";//минуты последнего коммента
 
-$currentDate=date("H:i");
+$currentDate = date( "H:i" );
 echo "$currentDate ";//текущее время
 
-sscanf($currentDate,"%d:%d" ,$hour, $min);
-$currentMin=$hour*60+$min;
+sscanf( $currentDate, "%d:%d", $hour, $min );
+$currentMin = $hour * 60 + $min;
 echo "$currentMin</br>";
-$comment_life=$currentMin-$minLastComm;//разница в минутах
+$comment_life = $currentMin - $minLastComm;//разница в минутах
 echo "DIFF = $comment_life </br>";
 echo "<a href=\"https://m.vk.com/wall-43932139_5970?post_add#post_add\">Add post</a></br>";
 $html->clear();//очистка памяти от объекта
-unset($html);
+unset( $html );
 
-if($comment_life>=20){
-  if($comment_life>=35 && $author_id!="id152223765"){
-	$message = "Last comment was added $comment_life minutes ago by user
+if ( $comment_life >= 20 ) {
+	if ( $comment_life >= 35 && $author_id != "id152223765" ) {
+		$message = "Last comment was added $comment_life minutes ago by user
   $first_name $last_name";
-	mail("good-1991@mail.ru","Chat", $message);
-  mail("feliasfogg1@yandex.ru", "Chat", $message);
-  mail("pavel.felias@gmail.com","Chat", $message);
-  mail("feliasfogg@outlook.com","Chat", $message);
-  if($comment_life>=45){
-  $sms=file_get_contents("http://sms.ru/sms/send?api_id=b8646699-0b12-1c14-ad92-7ab16971b8a1&to=375259466591&text=".urlencode(iconv("windows-1251","utf-8","Last comment was added $comment_life minutes ago")));    
-    }
-  }
-     	    $dbconnect = mysql_connect ($dbhost, $dbusername, $dbpass) or die("<p>Ошибка подключения к базе данных: " . mysql_error() . "</p>");
-          //говорим базе что записываем в нее все в utf8
-          mysql_query("SET NAMES 'utf8';"); 
-          mysql_query("SET CHARACTER SET 'utf8';"); 
-          mysql_query("SET SESSION collation_connection = 'utf8_general_ci';");
-          mysql_select_db($db_name) or die ("<p>Невозможно выбрать базу: " . mysql_error() . "</p>");
+		mail( "good-1991@mail.ru", "Chat", $message );
+		mail( "feliasfogg1@yandex.ru", "Chat", $message );
+		mail( "pavel.felias@gmail.com", "Chat", $message );
+		mail( "feliasfogg@outlook.com", "Chat", $message );
+		if ( $comment_life >= 45 ) {
+			$sms
+				= file_get_contents( "http://sms.ru/sms/send?api_id=b8646699-0b12-1c14-ad92-7ab16971b8a1&to=375259466591&text="
+				                     . urlencode( iconv( "windows-1251",
+						"utf-8",
+						"Last comment was added $comment_life minutes ago" ) ) );
+		}
+	}
+	$dbconnect = mysql_connect( $dbhost, $dbusername, $dbpass )
+	or die( "<p>Ошибка подключения к базе данных: " . mysql_error() . "</p>" );
+	//говорим базе что записываем в нее все в utf8
+	mysql_query( "SET NAMES 'utf8';" );
+	mysql_query( "SET CHARACTER SET 'utf8';" );
+	mysql_query( "SET SESSION collation_connection = 'utf8_general_ci';" );
+	mysql_select_db( $db_name ) or die ( "<p>Невозможно выбрать базу: "
+	                                     . mysql_error() . "</p>" );
 
-          $row=searchUser($author_id);
-          if($row){
-               $user_id=$row['id'];
-               if($user_id) addComment($user_id, $comment_life, $comment_time);
-               getCommentsCount($user_id);
-          } else {
-            addUser($first_name, $last_name, $author_id);
-            $row=searchUser($author_id);
-            $user_id=$row['id'];//получаем id вновьдобавленного пользователя
-            addComment($user_id, $comment_life, $comment_time);
-            getCommentsCount($user_id);
-          }
-  }
+	$row = searchUser( $author_id );
+	if ( $row ) {
+		$user_id = $row['id'];
+		if ( $user_id ) {
+			addComment( $user_id, $comment_life, $comment_time );
+		}
+		getCommentsCount( $user_id );
+	} else {
+		addUser( $first_name, $last_name, $author_id );
+		$row     = searchUser( $author_id );
+		$user_id = $row['id'];//получаем id вновьдобавленного пользователя
+		addComment( $user_id, $comment_life, $comment_time );
+		getCommentsCount( $user_id );
+	}
+}
 ?>
--- sample.php
+++ sample.php
@@ -1,93 +1,120 @@
 ﻿<?php
-     $boolCheckCookie = false;
-     if(isset($_COOKIE['first_name'])){ 
-          $username['first_name'] = $_COOKIE['first_name']; 
-          $username['last_name'] = $_COOKIE['last_name'];
-          $username['network'] = $_COOKIE['network'];
-          $username['identity'] = $_COOKIE['identity'];
-          $page_adress = $_COOKIE['page_adress'];
-          $boolCheckCookie = true;
-          echo "Отработали куки <br>";
-     }
-     else{
-          if($boolCheckCookie == false){
-               session_start();
-               if(isset($_SESSION['user'], $_SESSION['page_adress'])){
-               $username = $_SESSION['user'];
-               $page_adress = $_SESSION['page_adress'];
-               echo "Отработала сессия <br>";
-               }
-          }
-     }
+$boolCheckCookie = false;
+if ( isset( $_COOKIE['first_name'] ) ) {
+	$username['first_name'] = $_COOKIE['first_name'];
+	$username['last_name']  = $_COOKIE['last_name'];
+	$username['network']    = $_COOKIE['network'];
+	$username['identity']   = $_COOKIE['identity'];
+	$page_adress            = $_COOKIE['page_adress'];
+	$boolCheckCookie        = true;
+	echo "Отработали куки <br>";
+} else {
+	if ( $boolCheckCookie == false ) {
+		session_start();
+		if ( isset( $_SESSION['user'], $_SESSION['page_adress'] ) ) {
+			$username    = $_SESSION['user'];
+			$page_adress = $_SESSION['page_adress'];
+			echo "Отработала сессия <br>";
+		}
+	}
+}
 
-     require_once 'app_config.php'; //подключение файла с данными для входа в БД
+require_once 'app_config.php'; //подключение файла с данными для входа в БД
 
-     $comment = trim($_REQUEST['user_comment']);
-     if(!$comment) die("Error comment");
+$comment = trim( $_REQUEST['user_comment'] );
+if ( ! $comment ) {
+	die( "Error comment" );
+}
 
-     function searchActicle($page_adress){
-          $query = "SELECT id FROM news WHERE link = '{$page_adress}';"; 
-          $result = mysql_query($query) or die("<p>Невозможно получить адрес страницы: " . mysql_error() . "</p>");
-          $row = mysql_fetch_array($result);
-          $article_id = $row['id'];
-          return $article_id;
-     }
+function searchActicle( $page_adress ) {
+	$query = "SELECT id FROM news WHERE link = '{$page_adress}';";
+	$result = mysql_query( $query )
+	or die( "<p>Невозможно получить адрес страницы: " . mysql_error()
+	        . "</p>" );
+	$row        = mysql_fetch_array( $result );
+	$article_id = $row['id'];
 
-     function searchUser($username){//ищем юзера по url возвращаем в качестве результата всю строку row
-          $query="SELECT id, first_name, last_name, network_url FROM users WHERE first_name='{$username['first_name']}'".
-          " AND last_name='{$username['last_name']}' AND network_url = '{$username['identity']}'";//ищем есть ли такой же url в базе
-          $res=mysql_query($query) or die("<p>Невозможно сделать запрос поиска пользователя: " . mysql_error() . "</p>");
-          $row=mysql_fetch_array($res);//получение результата запроса из базы;
-          $user_id = $row['id'];//получаем id пользователя
-          return $user_id;
-     }
+	return $article_id;
+}
 
-     function addUser($username){//добавление пользователя
-          if($username['first_name']){
-               $query = "INSERT INTO users (first_name, last_name, network, network_url)" .
-               "VALUES ('{$username['first_name']}', '{$username['last_name']}', '{$username['network']}', '{$username['identity']}');";
-               $result = mysql_query($query) or die("<p>Невозможно добавить пользователя " . mysql_error() . "</p>");
-          }
-          else{
-               die("Ошибка: нет данных о пользователе\n");
-          }
-     }
+function searchUser(
+	$username
+) {//ищем юзера по url возвращаем в качестве результата всю строку row
+	$query
+		=
+		"SELECT id, first_name, last_name, network_url FROM users WHERE first_name='{$username['first_name']}'"
+		.
+		" AND last_name='{$username['last_name']}' AND network_url = '{$username['identity']}'";//ищем есть ли такой же url в базе
+	$res = mysql_query( $query )
+	or die( "<p>Невозможно сделать запрос поиска пользователя: " . mysql_error()
+	        . "</p>" );
+	$row     = mysql_fetch_array( $res );//получение результата запроса из базы;
+	$user_id = $row['id'];//получаем id пользователя
+	return $user_id;
+}
 
-     function addComment($article_id, $user_id, $comment){//добавляем комментарий
-          echo "$article_id ";
-          echo "$user_id ";
-          echo "$comment ";
-          $query= "INSERT INTO comments (news_id, user_id, comment) VALUES ('{$article_id}', '{$user_id}', '{$comment}');";
-          $res = mysql_query($query) or die("<p>Невозможно сделать запись комментария: " . mysql_error() . "</p>");
-          if($res) return true;
-          else return false;
-     }
+function addUser( $username ) {//добавление пользователя
+	if ( $username['first_name'] ) {
+		$query
+			=
+			"INSERT INTO users (first_name, last_name, network, network_url)" .
+			"VALUES ('{$username['first_name']}', '{$username['last_name']}', '{$username['network']}', '{$username['identity']}');";
+		$result = mysql_query( $query )
+		or die( "<p>Невозможно добавить пользователя " . mysql_error()
+		        . "</p>" );
+	} else {
+		die( "Ошибка: нет данных о пользователе\n" );
+	}
+}
 
-     if(isset($username)){
-          //коннект к базе
-          $dbconnect = mysql_connect ($dbhost, $dbusername, $dbpass) or die("<p>Ошибка подключения к базе данных: " . mysql_error() . "</p>");
-          //говорим базе что записываем в нее все в utf8
-          mysql_query("SET NAMES 'utf8';"); 
-          mysql_query("SET CHARACTER SET 'utf8';"); 
-          mysql_query("SET SESSION collation_connection = 'utf8_general_ci';");
-          mysql_select_db($db_name) or die ("<p>Невозможно выбрать базу: " . mysql_error() . "</p>");
-          
-          $article_id = searchActicle($page_adress); //Получаем идентификатор страницы на которой нужно разместить комментарий
-          $user_id=searchUser($username);//первоначально ищем пользователя
-          if($user_id){//пишем коммент
-               addComment($article_id, $user_id, $comment);
-          } 
-          else {//если пользователя нет - добавляем нового и пишем коммент
-            addUser($username);
-            $user_id=searchUser($username);
-            $comment_result = addComment($article_id, $user_id, $comment);
-            if(comment){echo "<br> Комментарий добавлен";}
-            else{echo "<br> Комментарий НЕ добавлен";}
-          }
+function addComment( $article_id, $user_id, $comment ) {//добавляем комментарий
+	echo "$article_id ";
+	echo "$user_id ";
+	echo "$comment ";
+	$query
+		= "INSERT INTO comments (news_id, user_id, comment) VALUES ('{$article_id}', '{$user_id}', '{$comment}');";
+	$res = mysql_query( $query )
+	or die( "<p>Невозможно сделать запись комментария: " . mysql_error()
+	        . "</p>" );
+	if ( $res ) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
-     } else echo ("Error: user not exist.");
-                    //$user['network'] - соц. сеть, через которую авторизовался пользователь
-                    //$user['identity'] - уникальная строка определяющая конкретного пользователя соц. сети
-                    //$user['first_name'] - имя пользователя
-                    //$user['last_name'] - фамилия пользователя
-     ?>
+if ( isset( $username ) ) {
+	//коннект к базе
+	$dbconnect = mysql_connect( $dbhost, $dbusername, $dbpass )
+	or die( "<p>Ошибка подключения к базе данных: " . mysql_error() . "</p>" );
+	//говорим базе что записываем в нее все в utf8
+	mysql_query( "SET NAMES 'utf8';" );
+	mysql_query( "SET CHARACTER SET 'utf8';" );
+	mysql_query( "SET SESSION collation_connection = 'utf8_general_ci';" );
+	mysql_select_db( $db_name ) or die ( "<p>Невозможно выбрать базу: "
+	                                     . mysql_error() . "</p>" );
+
+	$article_id
+		     = searchActicle( $page_adress ); //Получаем идентификатор страницы на которой нужно разместить комментарий
+	$user_id = searchUser( $username );//первоначально ищем пользователя
+	if ( $user_id ) {//пишем коммент
+		addComment( $article_id, $user_id, $comment );
+	} else {//если пользователя нет - добавляем нового и пишем коммент
+		addUser( $username );
+		$user_id        = searchUser( $username );
+		$comment_result = addComment( $article_id, $user_id, $comment );
+		if ( comment ) {
+			echo "<br> Комментарий добавлен";
+		} else {
+			echo "<br> Комментарий НЕ добавлен";
+		}
+	}
+
+} else {
+	echo( "Error: user not exist." );
+}
+//$user['network'] - соц. сеть, через которую авторизовался пользователь
+//$user['identity'] - уникальная строка определяющая конкретного пользователя соц. сети
+//$user['first_name'] - имя пользователя
+//$user['last_name'] - фамилия пользователя
+?>
