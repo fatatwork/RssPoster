508d4c85357c9f8b239630fa712ccb271df7a0a5
ukod <ukosname@mail.ru>
2015-03-03 15:35:50 +0300

    Подключение к БД вынесено в отдельный файл

--- funcLib.php
+++ funcLib.php
@@ -1,15 +1,5 @@
 <?
-	require_once 'pdo_mysql.php';
-	require_once 'app_config.php';
-	$dbconnect = pdo_connect( $dbhost, $dbusername, $dbpass )
-	or die( "<p>Ошибка подключения к базе данных: " . pdo_error() . "</p>" );
-	//говорим базе что записываем в нее все в utf8
-	pdo_query( "SET NAMES 'utf8';" );
-	pdo_query( "SET CHARACTER SET 'utf8';" );
-	pdo_query( "SET SESSION collation_connection = 'utf8_general_ci';" );
-	pdo_select_db( $db_name ) or die ( "<p>Невозможно выбрать базу: "
-	                                     . pdo_error() . "</p>" );
-
+require_once 'connectDB.php';
 function searchActicle( $page_adress ) {
 	$query = "SELECT id FROM news WHERE link = '{$page_adress}';";
 	$result = pdo_query( $query )
