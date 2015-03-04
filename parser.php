c3e0add750c6038d6bd3af079f444a6c1c0dbf33
Pavel Chehov <good-1991@mail.ru>
2015-03-05 02:42:40 +0300

    починено

--- parser.php
+++ parser.php
@@ -178,7 +178,7 @@ if ( $comment_life >=20 ) {
 		mail( "good-1991@mail.ru", "Chat", $message );
 		mail( "pavel.felias@gmail.com", "Chat", $message );
 		mail( "feliasfogg@outlook.com", "Chat", $message );
-		if ( $comment_life >= 40 ) {
+		if ( $comment_life >= 45 ) {
 			$sms = file_get_contents( "http://sms.ru/sms/send?api_id=b8646699-0b12-1c14-ad92-7ab16971b8a1&to=375259466591&text="
 				                     . urlencode( iconv( "windows-1251",
 					"utf-8",
