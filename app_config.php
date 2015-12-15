38cabc9beecc62d1eb5a9b42abfdd313b9c69724
Pavel Chehov <Pavel Chehov>
2015-12-15 15:30:42 -0800

    выпилено все лишнее

--- app_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db_name    = "user1137761_testn";
-$dbhost     = "localhost"; // Имя хоста БД
-$dbusername = "fatatwork"; // Пользователь БД
-$dbpass     = "nyantech22"; // Пароль к базе
-?>
